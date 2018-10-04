ed0a6fda16a234f81e4292974c6830e4c4961e2c
maram_rafe <maroomah1010@gmail.com>
2018-10-04 14:16:02 +0300

    finish project

--- read.js
+++ read.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
-$('#b2').click(History);
-})
+//styling using jquery.
+$('#head').css("background", "#009999");
+$('p').css("color", "#e6ffff")
+$('#text').css('font-size', '100px');
 
-var History = function(){
-	$('span').append('<h1> kgsslkj <h2>')
-}
-
+//declare an array of qutes and show them in a div.
 var arr = ["A reader lives a thousand lives before he dies", "We read to know we're not alone", "If one cannot enjoy reading a book over and over again, there is no use in reading it at all"];
+var i = 0;
+//use the click function to show the qutes.
+$('#text').click(function() {
+	if(i < arr.length) {
+	$('#qtsD').text(arr[i])
+	i++;
+	};
+});
